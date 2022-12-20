28f80851ef7f061ad0178e4b9d6f285997984239
Armandt van Zyl <armandtvz@gmail.com>
2022-12-20 17:30:41 +0200

    1TBS + prefer-const

--- src/js/body/theme.js
+++ src/js/body/theme.js
@@ -1,18 +1,15 @@
 'use strict';
 const body = document.querySelector('body');
 const current_theme = body.dataset.theme;
-if (current_theme === 'system' || ! current_theme)
-{
-    let dark = window.matchMedia('(prefers-color-scheme: dark)');
-    let light = window.matchMedia('(prefers-color-scheme: light)');
+if (current_theme === 'system' || ! current_theme) {
+    const dark = window.matchMedia('(prefers-color-scheme: dark)');
+    const light = window.matchMedia('(prefers-color-scheme: light)');
     const body = document.querySelector('body');
 
-    if (dark.matches)
-    {
+    if (dark.matches) {
         body.classList.add('dark-theme');
     }
-    if (light.matches)
-    {
+    if (light.matches) {
         body.classList.remove('dark-theme');
     }
 }
