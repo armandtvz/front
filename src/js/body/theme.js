0295b933ec81b6bd6ec013ef03a2140ffe6ebe61
Armandt van Zyl <armandtvz@gmail.com>
2022-12-20 17:31:42 +0200

    Fix redeclaration of variable

--- src/js/body/theme.js
+++ src/js/body/theme.js
@@ -4,7 +4,6 @@ const current_theme = body.dataset.theme;
 if (current_theme === 'system' || ! current_theme) {
     const dark = window.matchMedia('(prefers-color-scheme: dark)');
     const light = window.matchMedia('(prefers-color-scheme: light)');
-    const body = document.querySelector('body');
 
     if (dark.matches) {
         body.classList.add('dark-theme');
